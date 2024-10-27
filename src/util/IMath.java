87e7d332f2878cf61308f76e00d16791d35959fd
Dillon8775 <dilloneystrickland12@gmail.com>
2024-10-27 00:37:54 -0400

    More stuff

---
+++ src/util/IMath.java
@@ -0,0 +1,12 @@
+package util;
+
+public class IMath {
+
+    public static double toDecimal(double d) {
+        return d / 100;
+    }
+
+    public static double toPercentage(double d) {
+        return d * 100;
+    }
+}
